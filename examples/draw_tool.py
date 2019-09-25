97319e8bb92860fcfc660b46632eef64b24d2640
xxao <github@bymartin.cz>
2019-09-25 18:29:44 +0200

    Simple mouse tool example

--- examples/draw_tool.py
+++ examples/draw_tool.py
@@ -12,14 +12,18 @@ class AngleTool(pero.Tool):
         
         super(AngleTool, self).__init__(**overrides)
         
+        # init buffers
+        self._center = (0, 0)
+        self._radius = 0
+        self._is_active = False
+        self._dragging = None
+        
         # init color scale
         self._gradient = pero.Gradient(pero.colors.YlOrRd)
         
         # init glyphs
         self._arrow = pero.RayArrow(
-            x = 0,
-            y = 0,
-            length = 0,
+            start_head = pero.CircleHead(size=7),
             end_head = pero.CircleHead(size=7),
             line_color = pero.colors.Red,
             fill_color = pero.colors.Red)
@@ -28,7 +32,7 @@ class AngleTool(pero.Tool):
             inner_radius = 0,
             start_angle = 0,
             line_color = None,
-            fill_color = pero.colors.Red.opaque(0.3))
+            fill_alpha = 170)
     
     
     def on_size(self, evt):
@@ -40,6 +44,10 @@ class AngleTool(pero.Tool):
         cy = evt.height/2
         length = min(evt.width, evt.height)/2 - margin
         
+        # update buffers
+        self._center = (cx, cy)
+        self._radius = length
+        
         # update glyphs
         self._arrow(
             x = cx,
@@ -49,48 +57,111 @@ class AngleTool(pero.Tool):
         self._wedge(
             x = cx,
             y = cy,
-            outer_radius = length,
-            fill_alpha = 170)
+            outer_radius = length)
+    
+    
+    def on_mouse_leave(self, evt):
+        """Handles mouse-leave event."""
+        
+        self._is_active = False
+        evt.control.draw_overlay()
     
     
     def on_mouse_motion(self, evt):
         """Handles mouse-motion event."""
         
-        evt.control.draw_overlay(self._draw_angle, evt=evt)
+        # draw current wedge
+        if self._is_active:
+            evt.control.draw_overlay(self._draw_angle, evt=evt)
+        
+        # draw current cursor
+        else:
+            evt.control.draw_overlay(self._draw_position, evt=evt)
     
     
-    def _draw_angle(self, canvas, evt):
-        """Draws current angle."""
+    def on_mouse_down(self, evt):
+        """Handles mouse-button-down event."""
         
-        # get cursor position
-        x, y = evt.x_pos, evt.y_pos
-        
-        # get center coords
-        cx = self._arrow.x
-        cy = self._arrow.y
-        
-        # check coords
-        if cx == x and cy == y:
+        # skip if active
+        if self._is_active:
             return
+        
+        # get coords
+        x, y = evt.x_pos, evt.y_pos
+        length = pero.distance(self._center, (x, y))
+        
+        # skip if outside
+        if length > self._radius:
+            return
+        
+        # set active
+        self._is_active = True
+        
+        # remember dragging
+        self._dragging = (x, y)
+    
+    
+    def on_mouse_up(self, evt):
+        """Handles mouse-button-up event."""
+        
+        # cancel current action
+        self._is_active = False
+    
+    
+    def _draw_position(self, canvas, evt):
+        """Draws current cursor position."""
+        
+        # get coords
+        x, y = evt.x_pos, evt.y_pos
+        cx, cy = self._center
         
         # get angle
         rads = pero.angle((cx+1, cy), (cx, cy), (x, y))
         if rads < 0:
             rads += 2*math.pi
         
-        # draw glyphs
-        self._wedge.draw(canvas,
-            end_angle = rads,
-            fill_color = self._gradient.color_at(rads/(2*math.pi)))
-        
-        self._arrow.draw(canvas,
-            angle = 0)
-        
-        self._arrow.draw(canvas,
-            angle = rads)
+        # draw arrow
+        self._arrow.draw(canvas, angle=rads)
         
         # draw tooltip
-        tooltip = "Rads: %.2f\nDegs: %.0f" % (rads, pero.degs(rads))
+        tooltip = "Degs: %.0f\nRads: %.2f" % (pero.degs(rads), rads)
+        evt.control.draw_tooltip(canvas, x=x, y=y, text=tooltip)
+    
+    
+    def _draw_angle(self, canvas, evt):
+        """Draws current angle."""
+        
+        # get coords
+        x, y = evt.x_pos, evt.y_pos
+        cx, cy = self._center
+        
+        # get angle
+        rads = pero.angle(self._dragging, (cx, cy), (x, y))
+        start_angle = pero.angle((cx+1, cy), (cx, cy), self._dragging)
+        end_angle = pero.angle((cx+1, cy), (cx, cy), (x, y))
+        
+        # get direction
+        if evt.right_down:
+            clockwise = False
+            if rads > 0:
+                rads -= 2*math.pi
+        else:
+            clockwise = True
+            if rads < 0:
+                rads += 2*math.pi
+        
+        # draw glyphs
+        self._wedge.draw(canvas,
+            start_angle = start_angle,
+            end_angle = end_angle,
+            clockwise = clockwise,
+            fill_color = self._gradient.color_at(abs(rads)/(2*math.pi)))
+        
+        self._arrow.draw(canvas, angle=start_angle)
+        self._arrow.draw(canvas, angle=end_angle)
+        
+        # draw tooltip
+        tooltip = "Degs: %.0f\nRads: %.2f" % (pero.degs(rads), rads)
         evt.control.draw_tooltip(canvas, x=x, y=y, text=tooltip)
 
 
@@ -104,6 +175,15 @@ class DrawTest(pero.Graphics):
         super(DrawTest, self).__init__()
         
         # init glyphs
+        self._axis = pero.RadialAxis(
+            units = pero.DEG,
+            labels = [str(i) for i in range(0, 360, 30)],
+            label_rotation = pero.NATURAL,
+            major_ticks = [i for i in range(0, 360, 30)],
+            minor_ticks = [i for i in range(0, 360, 6)],
+            start_angle = pero.rads(0),
+            end_angle = pero.rads(360))
+        
         self._ray_grid = pero.RayGrid(
             line_color = pero.colors.LightGrey,
             ticks = tuple(range(0, 360, 30)),
@@ -150,6 +230,11 @@ class DrawTest(pero.Graphics):
             x = cx,
             y = cy,
             ticks = ticks)
+        
+        self._axis.draw(canvas,
+            x = cx,
+            y = cy,
+            radius = radius)
 
 
 # run test
@@ -157,6 +242,6 @@ if __name__ == '__main__':
     
     control = pero.Control()
     control.graphics = DrawTest()
-    control.cursor_tool = AngleTool()
+    control.main_tool = AngleTool()
     
     pero.debug(control, 'show', "Overlay Tool", 400, 400)
