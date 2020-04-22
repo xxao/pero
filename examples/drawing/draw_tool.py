9b601e8e603245c76feb343dcc9d4de9c83dbf0f
xxao <github@bymartin.cz>
2020-04-22 20:03:04 +0200

    Improved tool example

--- examples/drawing/draw_tool.py
+++ examples/drawing/draw_tool.py
@@ -13,11 +13,13 @@ class AngleTool(pero.Tool):
         
         super().__init__(**overrides)
         
-        # init buffers
+        # init values
         self._center = (0, 0)
         self._radius = 0
-        self._is_active = False
+        self._offset = 0
         self._dragging = None
+        self._is_measure = False
+        self._is_rotate = False
         
         # init color scale
         self._gradient = pero.Gradient(pero.colors.YlOrRd)
@@ -39,7 +41,7 @@ class AngleTool(pero.Tool):
     def on_size(self, evt):
         """Handles window size event."""
         
-        # get coords
+        # get position
         margin = 30
         cx = evt.width/2
         cy = evt.height/2
@@ -59,24 +61,54 @@ class AngleTool(pero.Tool):
         self._wedge.outer_radius = length
     
     
-    def on_mouse_leave(self, evt):
-        """Handles mouse-leave event."""
+    def on_mouse_down(self, evt):
+        """Handles mouse-button-down event."""
         
-        # cancel action and clear overlay
-        self._is_active = False
-        evt.control.draw_overlay()
+        # skip if active
+        if self._is_measure or self._is_rotate:
+            return
+        
+        # get position
+        x, y = evt.x_pos, evt.y_pos
+        length = pero.distance(self._center, (x, y))
+        
+        # start measure
+        if length <= self._radius:
+            self._is_measure = True
+        
+        # start rotate
+        else:
+            self._is_rotate = True
+            self._offset = evt.control.graphics.get_offset()
+        
+        # remember dragging
+        self._dragging = (x, y)
+    
+    
+    def on_mouse_up(self, evt):
+        """Handles mouse-button-up event."""
+        
+        # cancel actions
+        self._cancel_evt(evt)
     
     
     def on_mouse_motion(self, evt):
         """Handles mouse-motion event."""
         
-        # get coords
+        # get position
         x, y = evt.x_pos, evt.y_pos
         length = pero.distance(self._center, (x, y))
         
-        # draw current wedge
-        if self._is_active:
-            evt.control.draw_overlay(self._draw_angle, evt=evt, x=x, y=y)
+        # set cursor
+        self._set_cursor(evt, x, y)
+        
+        # measure angle
+        if self._is_measure:
+            evt.control.draw_overlay(self._draw_wedge, evt=evt, x=x, y=y)
+        
+        # rotate axis
+        elif self._is_rotate:
+            self._rotate_axis(evt, x, y)
         
         # draw current cursor
         elif length <= self._radius:
@@ -87,85 +119,67 @@ class AngleTool(pero.Tool):
             evt.control.draw_overlay()
     
     
-    def on_mouse_down(self, evt):
-        """Handles mouse-button-down event."""
+    def on_mouse_leave(self, evt):
+        """Handles mouse-leave event."""
         
-        # skip if active
-        if self._is_active:
-            return
-        
-        # get coords
-        x, y = evt.x_pos, evt.y_pos
-        length = pero.distance(self._center, (x, y))
-        
-        # skip if outside
-        if length > self._radius:
-            return
-        
-        # set active
-        self._is_active = True
-        
-        # remember dragging
-        self._dragging = (x, y)
-    
-    
-    def on_mouse_up(self, evt):
-        """Handles mouse-button-up event."""
-        
-        # cancel action and clear overlay
-        self._is_active = False
-        evt.control.draw_overlay()
+        # cancel actions
+        self._cancel_evt(evt)
     
     
     def on_touch_start(self, evt):
         """Handles touch-start event."""
         
         # skip if active
-        if self._is_active:
+        if self._is_measure or self._is_rotate:
             return
         
-        # get coords
+        # get position
         touch = evt.touches[0]
         x, y = touch.x_pos, touch.y_pos
         length = pero.distance(self._center, (x, y))
         
-        # skip if outside
-        if length > self._radius:
-            return
+        # start measure
+        if length <= self._radius:
+            self._is_measure = True
         
-        # set active
-        self._is_active = True
+        # start rotate
+        else:
+            self._is_rotate = True
+            self._offset = evt.control.graphics.get_offset()
         
         # remember dragging
         self._dragging = (x, y)
         
         # draw current cursor
-        evt.control.draw_overlay(self._draw_position, evt=evt, x=x, y=y)
+        if self._is_measure:
+            evt.control.draw_overlay(self._draw_position, evt=evt, x=x, y=y)
     
     
     def on_touch_end(self, evt):
         """Handles touch-end event."""
         
-        # cancel action and clear overlay
-        self._is_active = False
-        evt.control.draw_overlay()
+        # cancel actions
+        self._cancel_evt(evt)
     
     
     def on_touch_move(self, evt):
         """Handles touch-move event."""
         
         # skip if inactive
-        if not self._is_active:
+        if not self._is_measure and not self._is_rotate:
             return
         
-        # get coords
+        # get position
         touch = evt.touches[0]
         x, y = touch.x_pos, touch.y_pos
-        length = pero.distance(self._center, (x, y))
         
-        # draw current wedge
-        if length <= self._radius:
-            evt.control.draw_overlay(self._draw_angle, evt=evt, x=x, y=y)
+        # measure angle
+        if self._is_measure:
+            evt.control.draw_overlay(self._draw_wedge, evt=evt, x=x, y=y)
+        
+        # rotate axis
+        elif self._is_rotate:
+            self._rotate_axis(evt, x, y)
         
         # clear overlay
         else:
@@ -175,9 +189,79 @@ class AngleTool(pero.Tool):
     def on_touch_cancel(self, evt):
         """Handles touch-cancel event."""
         
-        # cancel action and clear overlay
-        self._is_active = False
+        # cancel actions
+        self._cancel_evt(evt)
+    
+    
+    def _set_cursor(self, evt, x, y):
+        """Sets cursor according to state and position."""
+        
+        # init cursor
+        cursor = pero.CURSOR_ARROW
+        
+        # get position
+        length = pero.distance(self._center, (x, y))
+        
+        # use direction arrows for axis rotation
+        if self._is_rotate or length > self._radius:
+            
+            # get angle
+            cx, cy = self._center
+            rads = pero.angle((cx + 1, cy), (cx, cy), (x, y))
+            if rads < 0:
+                rads += 2*math.pi
+            
+            # set cursor
+            if rads <= pero.rads(45):
+                cursor = pero.CURSOR_SIZENS
+            elif rads <= pero.rads(135):
+                cursor = pero.CURSOR_SIZEWE
+            elif rads <= pero.rads(225):
+                cursor = pero.CURSOR_SIZENS
+            elif rads <= pero.rads(315):
+                cursor = pero.CURSOR_SIZEWE
+            else:
+                cursor = pero.CURSOR_SIZENS
+        
+        # set cursor
+        evt.control.set_cursor(cursor)
+    
+    
+    def _cancel_evt(self, evt):
+        """Cancels all current event."""
+        
+        # cancel actions
+        self._is_measure = False
+        self._is_rotate = False
+        
+        # clear overlay
         evt.control.draw_overlay()
+    
+    
+    def _rotate_axis(self, evt, x, y):
+        """Rotates main axis."""
+        
+        # get angle
+        cx, cy = self._center
+        rads = pero.angle(self._dragging, (cx, cy), (x, y)) + self._offset
+        
+        # shift axis
+        evt.control.graphics.set_offset(rads)
+        
+        # redraw graphics
+        evt.control.refresh()
+        
+        # draw tooltip
+        if not evt.shift_down:
+            
+            # get total offset
+            rads = evt.control.graphics.get_offset()
+            if rads < 0:
+                rads += 2*math.pi
+            
+            # draw tooltip
+            tooltip = "Degs: %.0f\nRads: %.2f" % (pero.degs(rads), rads)
+            evt.control.draw_tooltip(x=x, y=y, text=tooltip)
     
     
     def _draw_position(self, canvas, evt, x, y):
@@ -186,20 +270,25 @@ class AngleTool(pero.Tool):
         # get angle
         cx, cy = self._center
         rads = pero.angle((cx+1, cy), (cx, cy), (x, y))
-        if rads < 0:
-            rads += 2*math.pi
         
         # draw glyphs
         self._arrow.draw(canvas, angle=rads)
         
         # draw tooltip
         if not evt.shift_down:
+            
+            # adjust angle
+            rads = rads - evt.control.graphics.get_offset()
+            if rads < 0:
+                rads += 2*math.pi
+            
+            # draw tooltip
             tooltip = "Degs: %.0f\nRads: %.2f" % (pero.degs(rads), rads)
             evt.control.draw_tooltip(canvas, x=x, y=y, text=tooltip)
     
     
-    def _draw_angle(self, canvas, evt, x, y):
-        """Draws current angle."""
+    def _draw_wedge(self, canvas, evt, x, y):
+        """Draws current wedge."""
         
         # get angle
         cx, cy = self._center
@@ -242,19 +331,24 @@ class DrawTest(pero.Graphics):
         
         super().__init__()
         
+        # set values
+        self._offset = 0
+        self._major_ticks = []
+        self._minor_ticks = []
+        
+        # calc ticks
+        self.set_offset(self._offset)
+        
         # init glyphs
         self._axis = pero.RadialAxis(
             units = pero.DEG,
             labels = [str(i) for i in range(0, 360, 30)],
             label_rotation = pero.TEXT_ROT_NATURAL,
-            major_ticks = [i for i in range(0, 360, 30)],
-            minor_ticks = [i for i in range(0, 360, 10)],
             start_angle = pero.rads(0),
             end_angle = pero.rads(360))
         
         self._ray_grid = pero.RayGrid(
             line_color = pero.colors.LightGrey,
-            ticks = tuple(range(0, 360, 30)),
             units = pero.ANGLE_DEG)
         
         self._rad_grid = pero.RadialGrid(
@@ -280,7 +374,7 @@ class DrawTest(pero.Graphics):
         # make radial ticks
         count = 5
         step = radius/count
-        ticks = [i*step for i in range(1, count+1)]
+        rad_ticks = [i*step for i in range(1, count+1)]
         
         # draw glyphs
         self._bgr.draw(canvas,
@@ -291,17 +385,38 @@ class DrawTest(pero.Graphics):
         self._ray_grid.draw(canvas,
             x = cx,
             y = cy,
-            length = radius)
+            length = radius,
+            ticks = self._major_ticks)
         
         self._rad_grid.draw(canvas,
             x = cx,
             y = cy,
-            ticks = ticks)
+            ticks = rad_ticks)
         
         self._axis.draw(canvas,
             x = cx,
             y = cy,
-            radius = radius)
+            radius = radius,
+            major_ticks = self._major_ticks,
+            minor_ticks = self._minor_ticks)
+    
+    
+    def get_offset(self, angle=0):
+        """Gets current angle offset in radians."""
+        
+        return self._offset
+    
+    
+    def set_offset(self, angle=0):
+        """Sets current angle offset in radians."""
+        
+        # set offset
+        self._offset = angle
+        
+        # calc ticks
+        offset = pero.degs(angle)
+        self._major_ticks = [i+offset for i in range(0, 360, 30)]
+        self._minor_ticks = [i+offset for i in range(0, 360, 10)]
 
 
 # run test
